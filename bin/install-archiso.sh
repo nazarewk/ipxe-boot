8459d6bdacfbd08239b539e5faaf88d93f753754
Krzysztof Nazarewski <nazarewk@gmail.com>
2018-01-04 19:24:57 +0100

    update archiso install

--- bin/install-archiso.sh
+++ bin/install-archiso.sh
@@ -4,4 +4,4 @@ release=${1:-latest}
 name=archiso
 mkdir -p media/${name}
 cd media/${name}
-wget -r -np -R "index.html" --relative https://mirrors.evowise.com/archlinux/iso/${release}/arch/ -nH --cut-dirs=3
+wget -r -np -R "index.html*" --relative https://mirrors.evowise.com/archlinux/iso/${release}/arch/ -nH --cut-dirs=3
