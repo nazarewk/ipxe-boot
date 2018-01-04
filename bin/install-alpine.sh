e8d6217f324ea1394f982f32c3b63f790391fe12
Krzysztof Nazarewski <nazarewk@gmail.com>
2018-01-04 18:34:27 +0100

    alpine nfs boot

--- bin/install-alpine.sh
+++ bin/install-alpine.sh
@@ -4,7 +4,7 @@ version=${2:-3.7.0}
 
 name=alpine-${release}
 filename=${name}-${version}-x86_64.iso
-mkdir -p media/${name}
+mkdir -p media/${name} media/alpine-nfsroot
 cd media/${name}
 wget http://dl-cdn.alpinelinux.org/alpine/v$(printf "%.3s" ${version})/releases/x86_64/${filename} -O ${filename}
 
