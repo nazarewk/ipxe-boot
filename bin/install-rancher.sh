1be6275cd548dd862be058f57b74be1864b00032
Krzysztof Nazarewski <nazarewk@gmail.com>
2018-01-08 10:23:52 +0100

    add rancher cloud configs

--- bin/install-rancher.sh
+++ bin/install-rancher.sh
@@ -4,8 +4,11 @@ release=${1:-latest}
 name=rancher
 url=http://releases.rancher.com/os/${release}
 
-mkdir -p media/${name}
-cd media/${name}
+file_dir=$(dirname "$(readlink -f "$0")")
+base_dir=$(readlink -f ${file_dir}/..)
+dir=${base_dir}/media/rancher
+mkdir -p ${dir} && cd ${dir}
+mkdir -p ${dir}/cc
 
 wget -N ${url}/vmlinuz
 wget -N ${url}/initrd
