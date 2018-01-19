9d2457df261102a64cd74646fd678b382e26a041
Krzysztof Nazarewski <nazarewk@gmail.com>
2018-01-19 22:36:06 +0100

    sprecyzowanie zakresu i dodanie przeglądu pojęć związanych z konteneryzacją

--- bin/kubernetes-rancher.sh
+++ bin/kubernetes-rancher.sh
@@ -1,16 +1,22 @@
 #!/usr/bin/env bash
-
+version=${1:-v2.0.0-alpha10}
 api_port=8080
 api_addr=192.168.56.1
 api_url="http://${api_addr}:${api_port}"
-docker run --rm --name rancher -d -p ${api_port}:8080 rancher/server:latest
-#docker logs -f rancher
-until curl -s "${api_url}"; do echo 'Waiting for Rancher to come up...'; sleep 5; done
+cmd () {
+  echo "$@"
+  "$@"
 
-# TODO: nie dziala
-# based on https://github.com/galal-hussein/Rancher-Ansible/blob/master/roles/rancher_reg/tasks/main.yml
-project_id=$(curl -s ${api_url}/v1/projects | jq '.data[0].id')
-echo PROJECT_ID=${project_id}
-token_data="$(curl -s "${api_url}/v1/registrationtokens?projectId=${project_id}" -X POST)"
-echo
-echo TOKEN_DATA="${token_data}"
+}
+cmd docker run --rm --name rancher -d -p ${api_port}:8080 rancher/server:${version}
+#docker logs -f rancher
+
+## TODO: nie dziala
+## based on https://github.com/galal-hussein/Rancher-Ansible/blob/master/roles/rancher_reg/tasks/main.yml
+#project_id=$(curl -s ${api_url}/v1/projects | jq '.data[0].id')
+#echo PROJECT_ID=${project_id}
+#token_data="$(curl -s "${api_url}/v1/registrationtokens?projectId=${project_id}" -X POST)"
+#echo
+#echo TOKEN_DATA="${token_data}"
+xdg-open ${api_url}
+docker attach rancher
