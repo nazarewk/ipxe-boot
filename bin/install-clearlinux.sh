97fbcf87368ff59475b01af38b8e771ca313992d
Krzysztof Nazarewski <nazarewk@gmail.com>
2018-01-26 12:27:19 +0100

    WIP: launching in zetis

---
+++ bin/install-clearlinux.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+release=${1:-standard}
+version=${2:-3.7.0}
+
+name=alpine-${release}
+filename=${name}-${version}-x86_64.iso
+mkdir -p media/${name} media/alpine-nfsroot
+cd media/${name}
+wget -N http://dl-cdn.alpinelinux.org/alpine/v$(printf "%.3s" ${version})/releases/x86_64/${filename} -O ${filename}
+
+7z x -y ${filename}
+
+# mkdir -p root
+# 7z x -y boot/modloop-hardened -oroot/.modloop
+# cp -R apks/ root/
